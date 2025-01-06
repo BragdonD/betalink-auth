ffa28c4ece4490e577b14dc1202533c35287f831
BragdonD <thomasduclos1012@gmail.com>
2025-01-06 13:59:26 +0100

    feat: edit sql migration file with default auth tables

---
+++ migrations/20250106123014_create_auth_tables.sql
@@ -0,0 +1,55 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE Users (
+    user_id uuid PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE HashAlgorithm (
+    hashAlgorithm VARCHAR(255) PRIMARY KEY
+)
+
+CREATE TABLE UsersLoginData (
+    user_id uuid PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    passwordHash VARCHAR(255) NOT NULL,
+    passwordSalt VARCHAR(255) NOT NULL,
+    hashAlgorithm VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (hashAlgorithm) REFERENCES HashAlgorithm(hashAlgorithm)
+)
+
+CREATE TABLE PasswordRecovery (
+    user_id uuid PRIMARY KEY,
+    recovery_token VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    used BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+)
+
+CREATE TABLE EmailVerification (
+    user_id uuid PRIMARY KEY,
+    verification_token VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    used BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+)
+
+CREATE TABLE ExternalLoginProviders (
+    provider_id uuid PRIMARY KEY,
+    provider_name VARCHAR(255) NOT NULL,
+    provider_endpoint TEXT NOT NULL,
+)
+
+CREATE TABLE UserLoginExternal (
+    user_id uuid PRIMARY KEY,
+    provider_id uuid NOT NULL,
+    provider_access_token VARCHAR(255) NOT NULL,
+    provider_refresh_token VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (provider_id) REFERENCES ExternalLoginProviders(provider_id)
+    
+)
+-- +goose StatementEnd
+
